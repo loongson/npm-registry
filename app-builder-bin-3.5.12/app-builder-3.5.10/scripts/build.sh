48b975bee063177617bd9638b53cd67f498d0ebb
Shi Pujin <shipujin@loongson.cn>
2023-02-09 14:58:13 +0800

    app-builder-bin-3.5.12: add npm-bin source
    Signed-off-by: Shi Pujin <shipujin@loongson.cn>

---
+++ app-builder-bin-3.5.12/app-builder-3.5.10/scripts/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -ex
+
+cd app-builder-bin
+
+rm -rf win
+rm -rf mac
+rm -rf linux
+
+mkdir mac
+GOOS=darwin GOARCH=amd64 go build -ldflags='-s -w' -o mac/app-builder_amd64 ..
+GOOS=darwin GOARCH=arm64 go build -ldflags='-s -w' -o mac/app-builder_arm64 ..
+ln -s app-builder_amd64 mac/app-builder
+
+mkdir -p linux/ia32
+GOOS=linux GOARCH=386 go build -ldflags='-s -w' -o linux/ia32/app-builder ..
+
+mkdir -p linux/x64
+GOOS=linux GOARCH=amd64 go build -ldflags='-s -w' -o linux/x64/app-builder ..
+
+mkdir -p linux/arm
+GOOS=linux GOARCH=arm go build -ldflags='-s -w' -o linux/arm/app-builder ..
+
+mkdir -p linux/arm64
+GOOS=linux GOARCH=arm64 go build -ldflags='-s -w' -o linux/arm64/app-builder ..
+
+mkdir -p win/ia32
+# set GOARCH=386
+GOOS=windows GOARCH=386 go build -o win/ia32/app-builder.exe ..
+
+mkdir -p win/x64
+# set GOARCH=amd64
+GOOS=windows GOARCH=amd64 go build -o win/x64/app-builder.exe ..
