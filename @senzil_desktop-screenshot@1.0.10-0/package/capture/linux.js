a34ff504ef3338d2189dd37f7772214d97b95681
Shi Pujin <shipujin@loongson.cn>
2023-03-09 20:57:53 +0800

    @senzil_desktop-screenshot@1.0.10-0: add LoongArch support
    Signed-off-by: Shi Pujin <shipujin@loongson.cn>

--- @senzil_desktop-screenshot@1.0.10-0/package/capture/linux.js
+++ @senzil_desktop-screenshot@1.0.10-0/package/capture/linux.js
@@ -14,7 +14,17 @@ module.exports = function(options, callback) {
     spawnOptions.env = options.env
   }
 
-  var scrot = childProcess.spawn(path.join(__dirname, 'bin', process.arch !== 'arm' ? 'scrot' : 'arm', 'scrot'), args, spawnOptions)
+  switch(process.arch) {
+  case 'arm':
+    var archDir = 'arm'
+    break;
+  case 'loong64':
+    var archDir = 'loongarch64'
+    break;
+  default:
+    var archDir = 'scrot'
+} 
+  var scrot = childProcess.spawn(path.join(__dirname, 'bin', archDir), args, spawnOptions)
   scrot.on('close', function(code) {
     if (code !== 0) {
       return callback('scrot failed', null)
